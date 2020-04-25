b74866d19a7d931eca15e9be1a1d0780ec8f7a9d
MichaelaKane <michaelakane@college.harvard.edu>
2020-04-25 18:40:58 +0900

    cleaning done for first sheet

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -5,7 +5,9 @@
 # This script will be for reading and cleaning all of the data from the Vox Machina Campaign of CritRoleStats:
 #    https://www.critrolestats.com/stats-vm
 
+library(tidyverse)
 library(gsheet) # what a godsend lets you download google sheet using url https://github.com/maxconway/gsheet
+library(naniar) # needed to replace "Unknown" with NA
 
 # Check for data/ directory
 ifelse(!dir.exists(here("data-raw/data")),
@@ -44,16 +46,9 @@ all_rolls <- gsheet2tbl('https://docs.google.com/spreadsheets/d/1OEg29XbL_YpO0m5
 ##### type -> Type of Roll (4)
 ##### total -> Total Value (5)
 ##### nat -> Natural Value (6)
-##### damage -> Damage Dealt (8), skip 7 Crit? because that's a useless varialbe
+##### damage -> Damage Dealt (8), skip Crit? (7) because we're making our own column for that
 ##### notes -> Notes (9)
-all_rolls <- all_rolls %>% select(Episode, # select columns by name in case order changes
-                     Time,
-                     Character,
-                     'Type of Roll', # need quotes for multiple word column names
-                     'Total Value',
-                     'Natural Value',
-                     'Damage Dealt',
-                     Notes) %>% rename(ep = Episode, # rename columns to be not horrible
+all_rolls <- all_rolls %>% rename(ep = Episode, # rename columns to be not horrible
                                   time = Time,
                                   char = Character,
                                   type = 'Type of Roll',
@@ -61,4 +56,18 @@ all_rolls <- all_rolls %>% select(Episode, # select columns by name in case orde
                                   nat = "Natural Value",
                                   damage = "Damage Dealt",
                                   notes = Notes)
+
+#### Now to clean. For starters, let's replace all "Unknown" with NA
+all_rolls <- all_rolls %>% replace_with_na_all(condition = ~.x == "Unknown")
+
+#### Now for the more complex problem of crits - natural 1's (Nat1) and natural 20s (Nat20).
+#### Not all of them have recorded total values, since crits automatically fail/succeed.
+#### But it'd be nice to note crits when they happen, so we'll use a binary crit variable
+##### Note this is noted only for attack rolls in the original file, but any crit rolled sounds more interesting,
+#####  so we'll write a new column.
+all_rolls$crit <- if_else((all_rolls$nat == 1 | all_rolls$nat == 20), 1, 0, 0) # 1 if crit, 0 otherwise
+
+#### Finally, we change the Nat1 and Nat20 in the total column to just 1 and 20
+all_rolls[all_rolls$total == "Nat1", 'total'] <- 1
+all_rolls[all_rolls$total == "Nat20", 'total'] <- 20
 
