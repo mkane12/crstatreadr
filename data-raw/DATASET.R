daa5e142e86f3c92f559b34db7939da7ddbe3098
MichaelaKane <michaelakane@college.harvard.edu>
2020-04-26 17:03:18 +0900

    fixed bug in reassigning Nat values to numeric

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -35,6 +35,7 @@ ifelse(!dir.exists(here("data-raw/data")),
 ##### total (int): total value of roll after adding any modifiers
 ##### nat (int): "natural" value of roll before adding modifiers
 ##### damage (int): amount of damage dealt if an attack roll (damage currently a string, will have to scrape and convert)
+##### crit (int): binary noting whether or not roll was a crit (nat 1 or nat 20)
 ##### notes (string): memos written by the dataset creator
 
 all_rolls <- gsheet2tbl('https://docs.google.com/spreadsheets/d/1OEg29XbL_YpO0m5JrLQpOPYTnxVsIg8iP67EYUrtRJg/edit')
@@ -48,14 +49,23 @@ all_rolls <- gsheet2tbl('https://docs.google.com/spreadsheets/d/1OEg29XbL_YpO0m5
 ##### nat -> Natural Value (6)
 ##### damage -> Damage Dealt (8), skip Crit? (7) because we're making our own column for that
 ##### notes -> Notes (9)
-all_rolls <- all_rolls %>% rename(ep = Episode, # rename columns to be not horrible
-                                  time = Time,
-                                  char = Character,
-                                  type = 'Type of Roll',
-                                  total = 'Total Value',
-                                  nat = "Natural Value",
-                                  damage = "Damage Dealt",
-                                  notes = Notes)
+all_rolls <- all_rolls %>%
+  select(Episode, # select columns by name in case order changes
+         Time,
+         Character,
+         'Type of Roll', # need quotes for multiple word column names
+         'Total Value',
+         'Natural Value',
+         'Damage Dealt',
+         Notes) %>%
+  rename(ep = Episode, # rename columns to be not horrible
+         time = Time,
+         char = Character,
+         type = 'Type of Roll',
+         total = 'Total Value',
+         nat = "Natural Value",
+         damage = "Damage Dealt",
+         notes = Notes)
 
 #### Now to clean. For starters, let's replace all "Unknown" with NA
 all_rolls <- all_rolls %>% replace_with_na_all(condition = ~.x == "Unknown")
@@ -63,11 +73,15 @@ all_rolls <- all_rolls %>% replace_with_na_all(condition = ~.x == "Unknown")
 #### Now for the more complex problem of crits - natural 1's (Nat1) and natural 20s (Nat20).
 #### Not all of them have recorded total values, since crits automatically fail/succeed.
 #### But it'd be nice to note crits when they happen, so we'll use a binary crit variable
-##### Note this is noted only for attack rolls in the original file, but any crit rolled sounds more interesting,
-#####  so we'll write a new column.
+##### This was noted only for attack rolls in the original file, but any crit rolled sounds more interesting, so we'll write a new column.
 all_rolls$crit <- if_else((all_rolls$nat == 1 | all_rolls$nat == 20), 1, 0, 0) # 1 if crit, 0 otherwise
 
 #### Finally, we change the Nat1 and Nat20 in the total column to just 1 and 20
-all_rolls[all_rolls$total == "Nat1", 'total'] <- 1
-all_rolls[all_rolls$total == "Nat20", 'total'] <- 20
+##### Need to check if na to avoid errors being thrown
+##### TODO: but maybe it'd be better to determine modifiers from previous rolls and add them? Sounds... hard
+all_rolls[!is.na(all_rolls$total) & all_rolls$total == "Nat1", 'total'] <- 1
+all_rolls[!is.na(all_rolls$total) & all_rolls$total == "Nat20", 'total'] <- 20
+
+
+
 
